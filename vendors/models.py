4ee7339b0752039b61254276eddcc97f5e189cd9
asim <measim8@gmail.com>
2020-07-30 11:16:20 +0530

    new app

--- vendors/models.py
+++ vendors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=20,null=False)
+    mobile = models.IntegerField()
+    email = models.CharField(max_length=50,null=False)
+    addres = models.TextField(null=True,blank=True )
+
+    def __str__(self):
+        return self.name
